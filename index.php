f4e920345aa14f97622bd9ee92be59fffdc62e24
Vadim Romanv <v.romanov@optimalgroup.ru>
2023-02-26 17:15:43 +0300

    update style notice

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
 $APPLICATION->SetTitle('Главная');
 use Bitrix\Main\Config\Option;
-Option::set('romanovv.contact', 'create_elements', false);
+//Option::set('romanovv.contact', 'create_elements', false);
 ?>
     <div class="page">
         <section id="promo" class="promo  section offset-header">
